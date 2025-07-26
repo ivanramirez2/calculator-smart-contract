4346b80e4f128421c6ebc6a42468a924b5b4dcdb
ivanramirez2 <126054440+ivanramirez2@users.noreply.github.com>
2025-07-26 22:14:03 +0200

    Added division function and improvements to the multiplication function

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -1,5 +1,6 @@
 // License
- SPDX-License-Identifier LGPL-3.0-only
+//SPDX-License-Identifier LGPL-3.0-only
+
 
 // Solidity version
 pragma solidity 0.8.24;
@@ -13,18 +14,29 @@ contract Calculator{
 
     // Modifiers
     modifier checkNumber(uint256 num1_){
-        require(num1_ == 10, Input must be equal to 10);
+        require(num1_ == 10, "Input must be equal to 10");
         _;
     }
 
 
     // Events
+
     event Addition(uint256 number1, uint256 number2, uint256 resultado);
+
     event Subtraction (uint256 number1, uint256 number2, uint256 resultado);
+
+    event Multiplication(uint256 number1, uint256 number2, uint256 resultado);
+
     event Multiplied(uint256 num1,uint256 resultado);
+
+    event Division(uint256 number1, uint256 number2, uint256 resultado);
+
+
+
 
 
     // External Functions
+
     function addition(uint256 num1_, uint256 num2_) public returns (uint256 resultado_) {
         resultado_ = num1_ + num2_;
         resultado = resultado_;
@@ -39,17 +51,24 @@ contract Calculator{
         emit Subtraction (num1_, num2_, resultado);
     }
 
-    function multiplier(uint256 num1_) public{
+    function multiplier(uint256 num1_, uint256 num2_) public returns (uint256 resultado_) {
+        resultado_ = num1_ * num2_;
+        resultado = resultado_;
 
-        resultado = resultado  num1_;
-
-        emit Multiplied(num1_, resultado);
+        emit Multiplication(num1_, num2_, resultado);
         
+    }
+
+    function division(uint256 num1_, uint256 num2_) public returns (uint256 resultado_){
+        resultado_ = num1_ / num2_;
+        resultado = resultado_;
+
+        emit Division(num1_, num2_, resultado);
     }
 
     function multiplier2(uint256 num1_) public checkNumber(num1_){
 
-        resultado = resultado  num1_;
+        resultado = resultado * num1_;
 
         emit Multiplied(num1_, resultado);
     }
